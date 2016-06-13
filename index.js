297646331ef594656133baaff53b67a8b08d98b2
Ivan Stroganov <ivan@chalkschools.com>
2016-06-13 11:42:54 -0700

    search bar ui added

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-import blah from 'place_finder';
-blah();
+var API_KEY = 'AIzaSyA-CgXTQCNbSq44Vdfd-USv1n0XSOvJ0ZM';
+
+var PlaceFinder = function($el, api_key) {
+  this.$el = $el;
+  this.$input = $el.find('input');
+  this.$search = $el.find('button');
+  this.url;
+  // tie event listeners.
+};
+
+
+navigator.geolocation.getCurrentPosition(function(position) {
+  // debugger;
+  //(position.coords.latitude, position.coords.longitude);
+});
